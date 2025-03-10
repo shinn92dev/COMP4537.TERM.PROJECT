47fbaa0456302cde817b102447fe681305c2c2ae
Anthony Shin <shinn92dev@gmail.com>
2025-03-10 15:53:15 -0700

    feat: migrate project to React Router

---
+++ frontend/app/routes/home.tsx
@@ -0,0 +1,14 @@
+import type { Route } from "./+types/home";
+
+export const meta = ({}: Route.MetaArgs) => {
+    return [
+        { title: "New React Router App" },
+        { name: "description", content: "Welcome to React Router!" },
+    ];
+};
+
+const Home = () => {
+    return <div>HOME</div>;
+};
+
+export default Home;
