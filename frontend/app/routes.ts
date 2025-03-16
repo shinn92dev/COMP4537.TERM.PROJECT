a611b2348fd4ccf37ed5e19bbb672ff1428247c3
Momona-Wada <144402784+Momona-Wada@users.noreply.github.com>
2025-03-15 21:53:12 -0700

    🎉init(dashboard):create dashboards for user and admin

--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -3,6 +3,6 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 export default [
     index("routes/home.tsx"),
     route("info", "routes/info.tsx"),
-    route("dashboard", "routes/dashboard.tsx"),
+    route("userDashboard", "routes/userDashboard.tsx"),
     route("login", "routes/login.tsx")
 ] satisfies RouteConfig;
