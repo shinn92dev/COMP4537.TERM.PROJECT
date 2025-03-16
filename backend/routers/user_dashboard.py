e71189593480f35b4885f9c28a833df7abdfca13
Momona-Wada <144402784+Momona-Wada@users.noreply.github.com>
2025-03-16 01:54:10 -0700

    🐛debug: comment out mock data to debug

--- backend/routers/user_dashboard.py
+++ backend/routers/user_dashboard.py
@@ -7,12 +7,12 @@ router = APIRouter()
 @router.get("/user-info", response_model=UserDashboardResponse)
 async def get_user_info(current_user: dict = Depends(get_current_user)):
     return {
-        # "name": current_user["username"],
-        # "email": current_user["email"],
-        # "requestLimit": current_user["request_limit"],
-        # "remainingRequests": current_user["remaining_requests"]
-        "name": "Test User",
-        "email": "testuser@test.com",
-        "requestLimit": 20,
-        "remainingRequests": 10
+        "name": current_user["username"],
+        "email": current_user["email"],
+        "requestLimit": current_user["request_limit"],
+        "remainingRequests": current_user["remaining_requests"]
+        # "name": "Test User",
+        # "email": "testuser@test.com",
+        # "requestLimit": 20,
+        # "remainingRequests": 10
     }
