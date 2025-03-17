08b68b542ad5a219106f37f52c563aa18e48194c
Momona-Wada <144402784+Momona-Wada@users.noreply.github.com>
2025-03-17 01:50:51 -0700

    debug: change allow-origins

---
+++ frontend/.react-router/types/+register.ts
@@ -0,0 +1,16 @@
+import "react-router";
+
+declare module "react-router" {
+  interface Register {
+    params: Params;
+  }
+}
+
+type Params = {
+  "/": {};
+  "/register": {};
+  "/info": {};
+  "/userDashboard": {};
+  "/adminDashboard": {};
+  "/login": {};
+};
