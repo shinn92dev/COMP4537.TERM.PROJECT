3bd82d50a203773bb6bbfd39c7d07e871df7b3aa
TatsunoriMarumo <t.maru0910@gmail.com>
2025-03-19 10:26:58 -0700

    fixed data type for sub in jwt

--- backend/utils/jwt_handler.py
+++ backend/utils/jwt_handler.py
@@ -56,7 +56,7 @@ async def get_current_user(request: Request):
 
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        user_id = payload.get("sub")
+        user_id = int(payload.get("sub"))
         if not user_id:
             raise credential_exception
         token_data = TokenData(user_id=user_id)
